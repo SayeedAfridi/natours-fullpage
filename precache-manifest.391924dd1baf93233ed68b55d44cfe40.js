1fec22b0ff78d269dc57432f18bf95984a0eb731
Sayeed Afridi <as.afridi563@gmail.com>
2019-10-07 02:40:06 +0600

    Updates

---
+++ precache-manifest.391924dd1baf93233ed68b55d44cfe40.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5d4b13c533efc573fb79677365828879",
+    "url": "/natours-fullpage/index.html"
+  },
+  {
+    "revision": "b8381881b98c319147d4",
+    "url": "/natours-fullpage/static/css/main.1a981070.chunk.css"
+  },
+  {
+    "revision": "c9984e5de331b3c772ab",
+    "url": "/natours-fullpage/static/js/2.e8040d8f.chunk.js"
+  },
+  {
+    "revision": "b8381881b98c319147d4",
+    "url": "/natours-fullpage/static/js/main.9da0441b.chunk.js"
+  },
+  {
+    "revision": "423a6e1451f46fa871f1",
+    "url": "/natours-fullpage/static/js/runtime-main.a8609b09.js"
+  },
+  {
+    "revision": "cd69d3552824dc8e99c3f2953de2735b",
+    "url": "/natours-fullpage/static/media/hero-small.cd69d355.jpg"
+  },
+  {
+    "revision": "6e09f4102b0d64adc38d21bd9c89a9a7",
+    "url": "/natours-fullpage/static/media/hero.6e09f410.jpg"
+  },
+  {
+    "revision": "3365abba6477c48e6be892cf6faa8f25",
+    "url": "/natours-fullpage/static/media/logo-green-2x.3365abba.png"
+  },
+  {
+    "revision": "40d4ef82deebd0521e91006d961ead40",
+    "url": "/natours-fullpage/static/media/logo-white.40d4ef82.png"
+  },
+  {
+    "revision": "7fdf2a886516e020e300ae6bd544ab7d",
+    "url": "/natours-fullpage/static/media/nat-1-large.7fdf2a88.jpg"
+  },
+  {
+    "revision": "2797561ff2a3de15c850f2744b73b6d1",
+    "url": "/natours-fullpage/static/media/nat-10.2797561f.jpg"
+  },
+  {
+    "revision": "7a25f1999a15760508f5e6131245aeac",
+    "url": "/natours-fullpage/static/media/nat-2-large.7a25f199.jpg"
+  },
+  {
+    "revision": "417d3d741a7a47b3d6ef0307228033a0",
+    "url": "/natours-fullpage/static/media/nat-3-large.417d3d74.jpg"
+  },
+  {
+    "revision": "61bbf12ae90a30cd432d77087921a02f",
+    "url": "/natours-fullpage/static/media/nat-4.61bbf12a.jpg"
+  },
+  {
+    "revision": "19f9a22f45a15fbf80d420862bcadffb",
+    "url": "/natours-fullpage/static/media/nat-5.19f9a22f.jpg"
+  },
+  {
+    "revision": "1ef6d764fc819e080d40aa72dfe03aa6",
+    "url": "/natours-fullpage/static/media/nat-6.1ef6d764.jpg"
+  },
+  {
+    "revision": "5f846d16e6be9a73e6eb73f620379af2",
+    "url": "/natours-fullpage/static/media/nat-7.5f846d16.jpg"
+  },
+  {
+    "revision": "14efd82b81787a9764b2cb7eb7afd3e8",
+    "url": "/natours-fullpage/static/media/nat-8.14efd82b.jpg"
+  },
+  {
+    "revision": "f556a2c3f37c228f95a5eac3fae8c4a7",
+    "url": "/natours-fullpage/static/media/video.f556a2c3.webm"
+  },
+  {
+    "revision": "ffcd876c5561db066c09f5922b384473",
+    "url": "/natours-fullpage/static/media/video.ffcd876c.mp4"
+  }
+]);
